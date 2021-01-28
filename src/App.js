0dd731a4b9f2bcc13d88c48907fe3d774024662c
Vasilije Knezevic <vasilije.upwork@gmail.com>
2021-01-28 19:08:03 +0100

    Navbar (additional styles added)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    
-<>
-    <Router>
-    <Navbar />
-    <Switch>
-      <Route path='/' exact />
-    </Switch>
-    </Router>
-</>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
